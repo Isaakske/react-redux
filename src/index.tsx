229152e4a0846cce702ce24da7b7c24a078d72cd
Isaak De Backer <isaak.de.backer@hotmail.com>
2017-08-21 15:32:38 +0200

    store meegeven aan provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,18 @@ import * as ReactDOM from "react-dom";
 import App from "./components/AppComponent";
 import {createStore} from "redux";
 import reducer from "./reducers/Reducer";
+import {Provider} from "react-redux";
+
+let store = createStore(reducer);
 
 let render = () => {
     ReactDOM.render(
-        <App />,
+        <Provider store={store}>
+            <App />
+        </Provider>,
         document.getElementById('root')
     );
 };
 
-let store = createStore(reducer);
 store.subscribe(render);
 render();
