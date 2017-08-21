df1edf9a278edeee4677ad09a0dd1e722457908d
Isaak De Backer <isaak.de.backer@hotmail.com>
2017-08-21 13:52:01 +0200

    betere state handling in taskreducer

--- src/reducers/Task.ts
+++ src/reducers/Task.ts
@@ -5,9 +5,8 @@ export default (state: List<Task> = <List<Task>>List.of(), action) => {
     switch(action.type) {
         case 'ADD_TASK':
             let task = {id: action.id, text: action.text};
-            state.push(task);
-
-            return state;
+            
+            return  state.push(task);
         default:
             return state;
     }
