9cacf67106a96b95d0a337487a474164c8656d97
Isaak De Backer <isaak.de.backer@hotmail.com>
2017-08-22 14:06:04 +0200

    zorgen dat ids in de list altijd overeen komen

--- src/reducers/TaskReducer.ts
+++ src/reducers/TaskReducer.ts
@@ -10,14 +10,13 @@ export default (state: List<Task> = <List<Task>>List.of(), action) => {
         case 'ADD_TASK':
             let task = {id: nextTaskId++, text: action.text, risks: <List<Risk>>List.of(), nextRiskId: 0};
 
-            return  state.push(task);
+            return  state.set(task.id, task);
         case 'ADD_RISK':
-            let newState = state;
-            let foundtask = newState.get(action.taskId);
+            let foundtask = state.get(action.taskId);
             let risk = {id: foundtask.nextRiskId++, text: action.text};
             foundtask.risks = foundtask.risks.push(risk);
 
-            return newState.set(action.taskId, foundtask);
+            return state.set(action.taskId, foundtask);
         default:
             return state;
     }
