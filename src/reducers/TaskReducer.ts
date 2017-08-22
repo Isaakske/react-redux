a74911fd48db26538bc0357d204e621bebed9d35
Isaak De Backer <isaak.de.backer@hotmail.com>
2017-08-22 11:16:57 +0200

    actions loggen

--- src/reducers/TaskReducer.ts
+++ src/reducers/TaskReducer.ts
@@ -5,6 +5,7 @@ import Risk from "../models/Risk";
 let nextTaskId = 0;
 
 export default (state: List<Task> = <List<Task>>List.of(), action) => {
+    console.log(action);
     switch(action.type) {
         case 'ADD_TASK':
             let task = {id: nextTaskId++, text: action.text, risks: <List<Risk>>List.of(), nextRiskId: 0};
